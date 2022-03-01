c130060bd9d406afff6cb207dd8de237220d35c5
rymsk-21 <seki.r621@gmail.com>
2022-03-02 01:47:26 +0900

    リスト 3.35終了

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,13 @@ module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.generators do |g|
+      g.test_framework :rspec,
+                       helper_specs: false,
+                       routing_specs: false,
+                       view_specs: false,
+                       controller_specs: false
+    end
+    config.generators.system_tests = nil
   end
 end
